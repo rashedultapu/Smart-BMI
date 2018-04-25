96254ef63d9d85b674fec27a0c0becf754c5cfe5
Rashedul Hussain Tapu <bangla.tapu@gmail.com>
2018-04-25 12:37:15 +0600

    first commit

---
+++ app/src/main/java/com/tapu/bmicalc/bmicalculator/ShowResult.java
@@ -0,0 +1,13 @@
+package com.tapu.bmicalc.bmicalculator;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ShowResult extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.show_result);
+    }
+}
